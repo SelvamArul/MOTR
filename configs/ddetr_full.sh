a96f4434d772a11062a0eccc7aaf7719490c93bd
Arul <finditarul@gmail.com>
2022-03-31 13:25:12 +0000

    ddetr single GPU

---
+++ configs/ddetr_full.sh
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2021 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Modified from Deformable DETR (https://github.com/fundamentalvision/Deformable-DETR)
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
+# ------------------------------------------------------------------------
+
+
+# for MOT17
+
+EXP_DIR=exps/ycbv_ddetr
+python ddetr_main.py \
+    --meta_arch ddetr \
+    --dataset_file ycbv\
+    --dataset_path '/home/cache/datasets/YCB_VIDEO_DATASET/YCB_Video_Dataset/data/'\
+    --dataset_desc_file_train '/home/user/periyasa/workspace/MOTR/datasets/ycbv_train_desc_train.txt'\
+    --dataset_desc_file_val '/home/user/periyasa/workspace/MOTR/datasets/ycbv_train_desc_val.txt'\
+    --epoch 200 \
+    --num_workers 0 \
+    --with_box_refine \
+    --lr_drop 100 \
+    --lr 2e-4 \
+    --lr_backbone 2e-5 \
+    --output_dir ${EXP_DIR} \
+    --batch_size 1 \
+    --sample_mode 'random_interval' \
+    --sample_interval 2 \
+    --sampler_steps 1 2 \
+    --sampler_lengths 8 8 8 \
+    --update_query_pos \
+    --merger_dropout 0 \
+    --dropout 0 \
+    --random_drop 0.1 \
+    --fp_ratio 0.3 \
+    --query_interaction_layer 'QIM' \
+    --extra_track_attn \
+    --data_txt_path_train ./datasets/data_path/mot17.train \
+    --data_txt_path_val ./datasets/data_path/mot17.train \
+    --mot_path '/home/cache/datasets/YCB_VIDEO_DATASET'
+
